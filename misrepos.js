15ce72899e4670dbb9d81634d2365b9e6a50a2cc
jesusgomezdias <74254861+jesusgomezdias@users.noreply.github.com>
2023-04-29 22:49:30 +0200

    Update misrepos.js

--- misrepos.js
+++ misrepos.js
@@ -1,27 +1,6 @@
 const reposContainer = document.querySelector("#repos-container");
 const reposError = document.querySelector("#repos-error");
 
-// Obtener el elemento del botón de menú y el menú desplegable
-const menuBtn = document.querySelector(".menu-btn");
-const menu = document.querySelector(".menu");
-
-// Inicializar el estado del menú
-let menuOpen = false;
-
-// Agregar un evento de clic al botón de menú
-menuBtn.addEventListener("click", () => {
-  // Cambiar el estado del menú
-  if (!menuOpen) {
-    menuBtn.classList.add("open");
-    menu.classList.add("show");
-    menuOpen = true;
-  } else {
-    menuBtn.classList.remove("open");
-    menu.classList.remove("show");
-    menuOpen = false;
-  }
-});
-
 // Obtener los repositorios de GitHub
 fetch("https://api.github.com/users/jesusgomezdias/repos")
   .then((response) => {
